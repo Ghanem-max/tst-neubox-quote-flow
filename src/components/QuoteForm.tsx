9ad26a02a3e0d8b6f30273c2b503610285132748
Ghanem-max <omar.ghanem@nauticlog.com>
2025-08-03 12:45:36 +0300

    Change the google sheet

--- src/components/QuoteForm.tsx
+++ src/components/QuoteForm.tsx
@@ -166,7 +166,7 @@ export const QuoteForm: React.FC = () => {
       //   body: JSON.stringify(submissionData),
       // });
       
-      const response = await fetch('https://script.google.com/macros/s/AKfycby82moPwaALiQ5Mabeakco7Q90AkIeedq9uGTHz1JhJaNiOAuirlGQdsnCz5mQTKM-9/exec', 
+      const response = await fetch('https://script.google.com/macros/s/AKfycbxRK0Q8V9CE_So1U8B-GHvaebD9Rqq7nHT8XXLe9zUYQ3126koPHXDhSlUsxJjyNWRl/exec', 
         {
           method: 'POST',
           mode: "no-cors", // suppresses CORS error, still sends data
